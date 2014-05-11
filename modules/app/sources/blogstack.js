c64c4a011243a68f656d969eb228c45d4c6149af
pico.dev <pico.dev@gmail.com>
2014-05-11 15:03:17 -0400

    Site updated Sunday, 11-05-2014 15:03

--- modules/app/sources/blogstack.js
+++ modules/app/sources/blogstack.js
@@ -3,6 +3,10 @@ define("app/sources/blogstack", [], function() {
   "name" : "Blog Stack",
   "posts" : [
     {
+      "url" : "/post/victorhckinthefreeworld/2014/05/indie-box-one-el-servidor-listo-para-usar",
+      "title" : "Indie Box one el servidor listo para usar"
+    },
+    {
       "url" : "/post/victorhckinthefreeworld/2014/05/fundacion-de-software-libre-recopilacion-de-noticias-de-abril-2014",
       "title" : "Fundación de software Libre. Recopilación de Noticias de Abril 2014"
     },
@@ -57,10 +61,6 @@ define("app/sources/blogstack", [], function() {
     {
       "url" : "/post/ochobitshacenunbyte/2014/04/instalar-y-configurar-nagios-en-servidor-debian-7-parte-i",
       "title" : "Instalar y configurar Nagios en servidor Debian 7 (Parte I)"
-    },
-    {
-      "url" : "/post/ochobitshacenunbyte/2014/04/una-tabla-de-elementos-con-mucho-humor",
-      "title" : "Una tabla de elementos con mucho humor"
     }
   ]
 }
--- modules/app/sources/victorhckinthefreeworld.js
+++ modules/app/sources/victorhckinthefreeworld.js
@@ -3,6 +3,10 @@ define("app/sources/victorhckinthefreeworld", [], function() {
   "name" : "Victorhck in the free world",
   "posts" : [
     {
+      "url" : "/post/victorhckinthefreeworld/2014/05/indie-box-one-el-servidor-listo-para-usar",
+      "title" : "Indie Box one el servidor listo para usar"
+    },
+    {
       "url" : "/post/victorhckinthefreeworld/2014/05/fundacion-de-software-libre-recopilacion-de-noticias-de-abril-2014",
       "title" : "Fundación de software Libre. Recopilación de Noticias de Abril 2014"
     }
