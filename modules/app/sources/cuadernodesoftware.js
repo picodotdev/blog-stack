59e8a1c41bd5e3887f5da31fa990671fde51b2f9
pico.dev <pico.dev@gmail.com>
2014-05-13 15:02:38 -0400

    Site updated Tuesday, 13-05-2014 15:02

---
+++ modules/app/sources/cuadernodesoftware.js
@@ -0,0 +1,47 @@
+define("app/sources/cuadernodesoftware", [], function() {
+	return {
+  "name" : "Cuaderno de software",
+  "posts" : [
+    {
+      "url" : "/post/cuadernodesoftware/2014/04/the-eye-of-the-storm",
+      "title" : "The eye of the storm"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2014/03/vagrant-chef-solo-para-crear-un-entorno-de-desarrollo-lamp",
+      "title" : "Vagrant + Chef solo para crear un entorno de desarrollo LAMP"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2014/03/utilizando-vagrant-para-tener-entornos-de-pruebas-lamp",
+      "title" : "Utilizando vagrant para tener entornos de pruebas (LAMP)"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2014/02/amazon-ec2-instancias-reservadas-y-precios",
+      "title" : "Amazon EC2, instancias reservadas y precios."
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2013/12/automatizar-la-gestion-y-aprovisionamiento-de-tu-infraestructura",
+      "title" : "Automatizar la gestión y aprovisionamiento de tu infraestructura"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2013/05/valladolid-1-de-junio-programania-from-the-trenches",
+      "title" : "Valladolid, 1 de Junio: Programania from the trenches"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2013/04/sobre-colegios-de-ingenieria-informatica",
+      "title" : "Sobre Colegios de Ingeniería Informática"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2013/02/algunos-pensamientos-sobre-story-points",
+      "title" : "Algunos pensamientos sobre Story Points"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2012/12/retrospectiva-personal-2012",
+      "title" : "Retrospectiva personal 2012"
+    },
+    {
+      "url" : "/post/cuadernodesoftware/2012/11/creacion-de-objetos-eficiente-en-javascript",
+      "title" : "Creación de objetos eficiente en JavaScript"
+    }
+  ]
+}
+});
