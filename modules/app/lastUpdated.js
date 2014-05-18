898378f6c35c2819b23224c3e6ef8b91ab6755ec
pico.dev <pico.dev@gmail.com>
2014-05-18 14:40:52 -0400

    Site updated Sunday, 18-05-2014 14:40

--- modules/app/lastUpdated.js
+++ modules/app/lastUpdated.js
@@ -1,5 +1,5 @@
 define("app/lastUpdated", [], function() {
 	return {
-		date: '201405181801+0000'
+		date: '201405181837+0000'
 	}
 });
---
+++ modules/app/sources/navegapolis.js
@@ -0,0 +1,35 @@
+define("app/sources/navegapolis", [], function() {
+	return {
+  "name" : "Navegapolis",
+  "posts" : [
+    {
+      "url" : "/post/navegapolis/2014/05/nueva-revision-del-libro-gestion-de-proyectos-scrum-manager",
+      "title" : "Nueva revisión del libro Gestión de proyectos Scrum Manager"
+    },
+    {
+      "url" : "/post/navegapolis/2014/04/niveles-de-scrum",
+      "title" : "Niveles de scrum"
+    },
+    {
+      "url" : "/post/navegapolis/2014/04/etica-empresarial",
+      "title" : "Ética empresarial"
+    },
+    {
+      "url" : "/post/navegapolis/2014/03/el-valor-de-las-personas-en-el-desarrollo-de-software",
+      "title" : "El valor de las personas en el desarrollo de software"
+    },
+    {
+      "url" : "/post/navegapolis/2014/02/compendio-de-ingenieria-del-software",
+      "title" : "Compendio de Ingeniería del Software"
+    },
+    {
+      "url" : "/post/navegapolis/2014/02/consejos-de-sei-para-encajar-en-sistemas-cmmi-la-contratacion-con-empresas-de-software-agiles",
+      "title" : "Consejos de SEI para encajar en sistemas CMMI la contratación con empresas de software ágiles."
+    },
+    {
+      "url" : "/post/navegapolis/2014/01/el-mito-de-las-evaluaciones-de-desempeno",
+      "title" : "El mito de las evaluaciones de desempeño"
+    }
+  ]
+}
+});
