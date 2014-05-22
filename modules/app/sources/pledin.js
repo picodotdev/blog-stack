a199295378bd4a3666bbc2244dcb5c7fbbaedd8f
pico.dev <pico.dev@gmail.com>
2014-05-22 14:04:13 -0400

    Site updated Thursday, 22-05-2014 14:04

---
+++ modules/app/sources/pledin.js
@@ -0,0 +1,47 @@
+define("app/sources/pledin", [], function() {
+	return {
+  "name" : "PLEDIN",
+  "posts" : [
+    {
+      "url" : "/post/pledin/2014/04/curso-fundamentos-de-cloud-computing-con-openstack-y-openshift",
+      "title" : "Curso: \u201cFundamentos de Cloud Computing con OpenStack y OpenShift\u201d"
+    },
+    {
+      "url" : "/post/pledin/2014/03/instalando-openstack-en-mi-portatil-2-parte-devstack",
+      "title" : "Instalando OpenStack en mi portátil (2ª parte): DevStack"
+    },
+    {
+      "url" : "/post/pledin/2014/02/simulando-switch-cisco-en-gns3",
+      "title" : "Simulando switch cisco en GNS3"
+    },
+    {
+      "url" : "/post/pledin/2014/02/instalando-openstack-en-mi-portatil",
+      "title" : "Instalando OpenStack en mi portátil"
+    },
+    {
+      "url" : "/post/pledin/2014/02/trabajando-con-switch-en-gns3-vlan-y-trunk",
+      "title" : "Trabajando con switch en GNS3: VLAN y Trunk"
+    },
+    {
+      "url" : "/post/pledin/2014/01/por-que-openstack-software-libre-para-la-nube",
+      "title" : "¿Por qué OpenStack? Software libre para la nube"
+    },
+    {
+      "url" : "/post/pledin/2014/01/openwebinars-por-que-openstack-software-libre-para-la-nube",
+      "title" : "OpenWebinars \u2013 ¿Por qué OpenStack? Software libre para la nube"
+    },
+    {
+      "url" : "/post/pledin/2013/11/configurar-subdominios-en-bind9",
+      "title" : "Configurar subdominios en bind9"
+    },
+    {
+      "url" : "/post/pledin/2013/11/configuracion-de-un-servidor-dns-esclavo-2-parte",
+      "title" : "Configuración de un servidor DNS esclavo (2ª parte)"
+    },
+    {
+      "url" : "/post/pledin/2013/11/gns3-anadiendo-hosts-a-nuestras-topologias",
+      "title" : "GNS3, añadiendo hosts a nuestras topologías"
+    }
+  ]
+}
+});
