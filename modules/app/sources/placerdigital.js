2c056aaea80162b9dad4268f5de889349df26ce4
pico.dev <pico.dev@gmail.com>
2014-05-23 17:14:21 -0400

    Site updated Friday, 23-05-2014 17:14

--- modules/app/sources/placerdigital.js
+++ modules/app/sources/placerdigital.js
@@ -3,6 +3,10 @@ define("app/sources/placerdigital", [], function() {
   "name" : "Placer digital",
   "posts" : [
     {
+      "url" : "/post/placerdigital/2013/03/felices-15-anos-mozilla-gracias-por-cambiar-el-mundo",
+      "title" : "¡Felices 15 años, Mozilla! Gracias por cambiar el mundo"
+    },
+    {
       "url" : "/post/placerdigital/2013/04/9-acronimos-en-internet-asap-lol-wtf-fyi-aka-y-otros",
       "title" : "9 acrónimos en Internet: ASAP, LOL, WTF, FYI, AKA y otros"
     },
@@ -11,28 +15,20 @@ define("app/sources/placerdigital", [], function() {
       "title" : "Firefox sigue mejorando la experiencia de navegación de los usuarios"
     },
     {
-      "url" : "/post/placerdigital/2013/03/felices-15-anos-mozilla-gracias-por-cambiar-el-mundo",
-      "title" : "¡Felices 15 años, Mozilla! Gracias por cambiar el mundo"
-    },
-    {
-      "url" : "/post/placerdigital/2013/06/como-remover-las-inutiles-imagenes-de-los-remitentes-en-gmail-para-android",
-      "title" : "Cómo remover las inútiles imágenes de los remitentes en Gmail para Android"
+      "url" : "/post/placerdigital/2013/06/como-extrano-el-acuerdo-entre-google-y-twitter-para-resultados-en-tiempo-real",
+      "title" : "Cómo extraño el acuerdo entre Google y Twitter para resultados en tiempo real"
     },
     {
       "url" : "/post/placerdigital/2013/05/como-saber-que-tecnologias-usa-un-sitio-web",
       "title" : "Cómo saber qué tecnologías usa un sitio web"
     },
     {
-      "url" : "/post/placerdigital/2013/06/como-extrano-el-acuerdo-entre-google-y-twitter-para-resultados-en-tiempo-real",
-      "title" : "Cómo extraño el acuerdo entre Google y Twitter para resultados en tiempo real"
+      "url" : "/post/placerdigital/2013/06/como-remover-las-inutiles-imagenes-de-los-remitentes-en-gmail-para-android",
+      "title" : "Cómo remover las inútiles imágenes de los remitentes en Gmail para Android"
     },
     {
       "url" : "/post/placerdigital/2014/04/como-bajar-o-disminuir-el-peso-de-una-o-varias-imagenes-de-forma-online",
       "title" : "Cómo bajar o disminuir el peso de una o varias imágenes de forma online"
-    },
-    {
-      "url" : "/post/placerdigital/2014/03/problema-de-accesibilidad-en-ventana-de-bloqueo-de-windows",
-      "title" : "Problema de accesibilidad en ventana de bloqueo de Windows"
     },
     {
       "url" : "/post/placerdigital/2013/10/como-hacer-que-un-tweet-se-borre-automaticamente-despues-de-un-tiempo-definido",
@@ -41,6 +37,10 @@ define("app/sources/placerdigital", [], function() {
     {
       "url" : "/post/placerdigital/2014/02/por-que-el-gobierno-y-el-sistema-educativo-deben-preferir-software-libre",
       "title" : "Por qué el gobierno y el sistema educativo DEBEN preferir software libre"
+    },
+    {
+      "url" : "/post/placerdigital/2014/03/problema-de-accesibilidad-en-ventana-de-bloqueo-de-windows",
+      "title" : "Problema de accesibilidad en ventana de bloqueo de Windows"
     }
   ]
 }
