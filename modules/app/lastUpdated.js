c73ed4a77e884dc4ad1d2bc69b351f0755d1fcfb
pico.dev <pico.dev@gmail.com>
2014-06-04 15:02:50 -0400

    Site updated Wednesday, 04-06-2014 15:02

--- modules/app/lastUpdated.js
+++ modules/app/lastUpdated.js
@@ -1,5 +1,5 @@
 define("app/lastUpdated", [], function() {
 	return {
-		date: '201406041801+0000'
+		date: '201406041901+0000'
 	}
 });
---
+++ modules/app/sources/elblogdejose.js
@@ -0,0 +1,47 @@
+define("app/sources/elblogdejose", [], function() {
+	return {
+  "name" : "El blog de Jose",
+  "posts" : [
+    {
+      "url" : "/post/elblogdejose/2014/03/mejorando-mi-blog-wordpress-parte-ii",
+      "title" : "Mejorando mi blog WordPress, Parte II"
+    },
+    {
+      "url" : "/post/elblogdejose/2014/03/mejorando-mi-blog-wordpress",
+      "title" : "Mejorando mi blog WordPress"
+    },
+    {
+      "url" : "/post/elblogdejose/2012/07/primeros-pasos-con-maven-parte-i",
+      "title" : "Primeros pasos con maven Parte I"
+    },
+    {
+      "url" : "/post/elblogdejose/2012/08/caused-by-java-io-ioexception-derinputstream-getlength-lengthtag-1-9-too-big",
+      "title" : "Caused by: java.io.IOException: DerInputStream.getLength(): lengthTag=109, too big."
+    },
+    {
+      "url" : "/post/elblogdejose/2014/02/plugins-wordpress-para-la-ley-de-cookies-espanola",
+      "title" : "Plugins WordPress para la ley de cookies española"
+    },
+    {
+      "url" : "/post/elblogdejose/2013/10/hadoop-beginner-s-guide",
+      "title" : "Hadoop Beginner\u2019s Guide"
+    },
+    {
+      "url" : "/post/elblogdejose/2013/08/como-ver-peliculas-desde-el-ipad-almacenadas-en-dropbox",
+      "title" : "Como ver películas desde el iPad almacenadas en Dropbox"
+    },
+    {
+      "url" : "/post/elblogdejose/2013/07/nuevo-juguete-la-raspberry-pi",
+      "title" : "Nuevo juguete, La Raspberry Pi"
+    },
+    {
+      "url" : "/post/elblogdejose/2012/08/decompilador-java",
+      "title" : "Decompilador Java"
+    },
+    {
+      "url" : "/post/elblogdejose/2012/07/mi-primera-aplicacion-en-ios",
+      "title" : "Mi primera aplicación en iOS"
+    }
+  ]
+}
+});
