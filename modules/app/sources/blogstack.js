d89f19a1cead89fb1bf139e88061a5d860a1541e
pico.dev <pico.dev@gmail.com>
2014-06-21 05:02:54 -0400

    Site updated Saturday, 21-06-2014 05:02

--- modules/app/sources/blogstack.js
+++ modules/app/sources/blogstack.js
@@ -3,16 +3,28 @@ define("app/sources/blogstack", [], function() {
   "name" : "Blog Stack",
   "posts" : [
     {
-      "url" : "/post/blogbitix/2014/06/iniciar-rapidamente-aplicacion-con-apache-tapestry",
-      "title" : "Iniciar rápidamente aplicación con Apache Tapestry"
+      "url" : "/post/laborategia/2014/04/kde-connect-o-porque-cada-dia-siento-mas-amor-hacia-kde-y-sus-locos-desarrolladores",
+      "title" : "KDE Connect: o porqué cada día siento mas amor hacia KDE y sus (locos) desarrolladores"
     },
     {
       "url" : "/post/laborategia/2014/05/esteganografia-por-software-en-gnu-linux-oculta-ficheros-en-otros-ficheros",
       "title" : "Esteganografía por software en GNU/Linux; Oculta ficheros en otros ficheros"
     },
     {
-      "url" : "/post/laborategia/2014/04/kde-connect-o-porque-cada-dia-siento-mas-amor-hacia-kde-y-sus-locos-desarrolladores",
-      "title" : "KDE Connect: o porqué cada día siento mas amor hacia KDE y sus (locos) desarrolladores"
+      "url" : "/post/laborategia/2014/05/esteganografia-a-traves-de-nuestro-smartphone-android-una-colaboracion-de-laborategia-net",
+      "title" : "Esteganografía a través de nuestro smartphone Android: una colaboración de laborategia.net"
+    },
+    {
+      "url" : "/post/laborategia/2014/06/recuperando-viejos-articulos-herramientas-para-la-productividad-manejo-remoto-de-libreoffice",
+      "title" : "Recuperando viejos artículos: Herramientas para la productividad: manejo remoto de LibreOffice"
+    },
+    {
+      "url" : "/post/laborategia/2014/06/the-deep-web-los-suburbios-de-internet",
+      "title" : "The Deep Web; ¿los suburbios de Internet?"
+    },
+    {
+      "url" : "/post/blogbitix/2014/06/iniciar-rapidamente-aplicacion-con-apache-tapestry",
+      "title" : "Iniciar rápidamente aplicación con Apache Tapestry"
     },
     {
       "url" : "/post/laborategia/2014/06/libremente-cc-administracion-basada-en-la-relacion-con-los-clientes-para-organizaciones-del-3-sector",
@@ -39,10 +51,6 @@ define("app/sources/blogstack", [], function() {
       "title" : "Fundación de software Libre. Recopilación de Noticias de Mayo 2014"
     },
     {
-      "url" : "/post/laborategia/2014/06/the-deep-web-los-suburbios-de-internet",
-      "title" : "The Deep Web; ¿los suburbios de Internet?"
-    },
-    {
       "url" : "/post/placerdigital/2014/06/acelerar-tu-pagina-web-retardando-la-carga-de-ciertos-contenidos-secundarios",
       "title" : "Acelerar tu página web retardando la carga de ciertos contenidos secundarios"
     },
@@ -53,14 +61,6 @@ define("app/sources/blogstack", [], function() {
     {
       "url" : "/post/variablenotfound/2014/06/ejecutar-tareas-en-segundo-plano-en-asp-net-4-5-2",
       "title" : "Ejecutar tareas en segundo plano en ASP.NET 4.5.2"
-    },
-    {
-      "url" : "/post/laborategia/2014/06/recuperando-viejos-articulos-herramientas-para-la-productividad-manejo-remoto-de-libreoffice",
-      "title" : "Recuperando viejos artículos: Herramientas para la productividad: manejo remoto de LibreOffice"
-    },
-    {
-      "url" : "/post/arquitecturajava/2014/06/java-websockets",
-      "title" : "Java WebSockets"
     }
   ]
 }
--- modules/app/sources/laborategia.js
+++ modules/app/sources/laborategia.js
@@ -3,24 +3,28 @@ define("app/sources/laborategia", [], function() {
   "name" : "Laborategia",
   "posts" : [
     {
-      "url" : "/post/laborategia/2014/05/esteganografia-por-software-en-gnu-linux-oculta-ficheros-en-otros-ficheros",
-      "title" : "Esteganografía por software en GNU/Linux; Oculta ficheros en otros ficheros"
-    },
-    {
       "url" : "/post/laborategia/2014/04/kde-connect-o-porque-cada-dia-siento-mas-amor-hacia-kde-y-sus-locos-desarrolladores",
       "title" : "KDE Connect: o porqué cada día siento mas amor hacia KDE y sus (locos) desarrolladores"
     },
     {
-      "url" : "/post/laborategia/2014/06/libremente-cc-administracion-basada-en-la-relacion-con-los-clientes-para-organizaciones-del-3-sector",
-      "title" : "[Libremente.cc] Administración basada en la relación con los clientes para organizaciones del 3. sector"
+      "url" : "/post/laborategia/2014/05/esteganografia-por-software-en-gnu-linux-oculta-ficheros-en-otros-ficheros",
+      "title" : "Esteganografía por software en GNU/Linux; Oculta ficheros en otros ficheros"
+    },
+    {
+      "url" : "/post/laborategia/2014/05/esteganografia-a-traves-de-nuestro-smartphone-android-una-colaboracion-de-laborategia-net",
+      "title" : "Esteganografía a través de nuestro smartphone Android: una colaboración de laborategia.net"
+    },
+    {
+      "url" : "/post/laborategia/2014/06/recuperando-viejos-articulos-herramientas-para-la-productividad-manejo-remoto-de-libreoffice",
+      "title" : "Recuperando viejos artículos: Herramientas para la productividad: manejo remoto de LibreOffice"
     },
     {
       "url" : "/post/laborategia/2014/06/the-deep-web-los-suburbios-de-internet",
       "title" : "The Deep Web; ¿los suburbios de Internet?"
     },
     {
-      "url" : "/post/laborategia/2014/06/recuperando-viejos-articulos-herramientas-para-la-productividad-manejo-remoto-de-libreoffice",
-      "title" : "Recuperando viejos artículos: Herramientas para la productividad: manejo remoto de LibreOffice"
+      "url" : "/post/laborategia/2014/06/libremente-cc-administracion-basada-en-la-relacion-con-los-clientes-para-organizaciones-del-3-sector",
+      "title" : "[Libremente.cc] Administración basada en la relación con los clientes para organizaciones del 3. sector"
     },
     {
       "url" : "/post/laborategia/2014/05/metadata-anonymisation-toolkit-limpieza-de-metadatos-en-los-confines-de-tu-hd",
