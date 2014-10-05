39be4ae22904a2733841f5e879872cc0e1d7e7bd
pico.dev <pico.dev@gmail.com>
2014-10-05 12:26:05 -0400

    Site updated Sunday, 05-10-2014 12:26

---
+++ modules/app/sources/elblogdeadrianarroyo.js
@@ -0,0 +1,11 @@
+define("app/sources/elblogdeadrianarroyo", [], function() {
+	return {
+  "name" : "El blog de Adrián Arroyo",
+  "posts" : [
+    {
+      "url" : "/post/elblogdeadrianarroyo/2014/08/la-odisea-de-una-aplicacion-comercial-hecha-en-ogre-y-cegui",
+      "title" : "La Odisea de una aplicación comercial hecha en Ogre y CEGUI"
+    }
+  ]
+}
+});
