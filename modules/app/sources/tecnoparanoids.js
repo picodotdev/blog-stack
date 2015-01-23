a16e8973969a2b3ec19d7b7d583500e9d9318c17
pico.dev <pico.dev@gmail.com>
2015-01-23 16:35:53 -0500

    Site updated Friday, 23-01-2015 16:35

---
+++ modules/app/sources/tecnoparanoids.js
@@ -0,0 +1,47 @@
+define("app/sources/tecnoparanoids", [], function() {
+	return {
+  "name" : "TecnoParanoids",
+  "posts" : [
+    {
+      "url" : "/post/tecnoparanoids/2015/01/tecnoparanoids-en-los-domingosnegros-de-elbinario-net",
+      "title" : "TecnoParanoids en los #DOMINGOSNEGROS de elbinario.net"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/12/tecnoparanoids-n-24-noticias-de-fin-de-ano",
+      "title" : "TecnoParanoids nº24: Noticias de fin de año"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/12/tecnoparanoids-n-23-tecnologia-y-guerras",
+      "title" : "TecnoParanoids nº23: Tecnología y guerras"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/11/tecnoparanoids-22-contrainformacion-tecnologica",
+      "title" : "TECNOPARANOIDS 22 CONTRAINFORMACIÓN TECNOLÓGICA"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/11/tecnoparanoids-21-hackers-del-kgb-en-un-macdonalds",
+      "title" : "TECNOPARANOIDS 21 HACKERS DEL KGB EN UN MACDONALDS "
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/11/tecnoparanoids-2-autistici-inventati",
+      "title" : "TECNOPARANOIDS 20 Autistici/Inventati"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/11/tecnoparanoids-19-firefox-os",
+      "title" : "TECNOPARANOIDS 19 Firefox OS"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/10/tecnoparanoids-n-18-findenegro-hackmeeting-y-divagaciones",
+      "title" : "TecnoParanoids nº18: FINDENEGRO, Hackmeeting y divagaciones."
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/10/tecnoparanoids-n-17-entrevista-a-siroco-sobre-el-hackmeeting-marinaleda-2-14-y-otros-proyectos",
+      "title" : "TecnoParanoids nº17: Entrevista a siroco sobre el Hackmeeting Marinaleda 2014 y otros proyectos"
+    },
+    {
+      "url" : "/post/tecnoparanoids/2014/09/tecnoparanoids-n-16-codecs-de-audio-con-la-idioteca",
+      "title" : "TecnoParanoids nº16: Codecs de Audio con La Idioteca"
+    }
+  ]
+}
+});
