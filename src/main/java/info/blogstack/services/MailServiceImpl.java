a35389718721342a3c4b2d1571f92130fcf20093
pico.dev <pico.dev@gmail.com>
2015-04-17 19:23:43 +0200

    Add com.sun.mail:javax.mail runtime dependency

--- src/main/java/info/blogstack/services/MailServiceImpl.java
+++ src/main/java/info/blogstack/services/MailServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
