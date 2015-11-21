f9c62c42ee39f9cb430613da3d8c20c621cdd522
pico.dev <pico.dev@gmail.com>
2015-11-21 14:27:42 +0100

    Bitácora #26: SYSADMIT Actualización librerías

--- src/main/dist/liquibase.sh
+++ src/main/dist/liquibase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-misc/liquibase/liquibase --classpath=misc/libs/h2-1.4.185.jar --driver=org.h2.Driver --changeLogFile=misc/database/changelog.xml --url=jdbc:h2:./misc/database/app --username=sa --password=sa update
+misc/liquibase/liquibase --classpath=misc/libs/h2-1.4.190.jar --driver=org.h2.Driver --changeLogFile=misc/database/changelog.xml --url=jdbc:h2:./misc/database/app --username=sa --password=sa update
